4a118c37830b4b3275a3605cd087300bce7c7230
srpingcat <15868107710@139.com>
2022-07-11 17:19:13 +0800

    no message

--- src/main/java/org/springcat/logcdr/file2db/impl/DemoFile2DbWorker.java
+++ src/main/java/org/springcat/logcdr/file2db/impl/DemoFile2DbWorker.java
@@ -1,12 +1,37 @@
 package org.springcat.logcdr.file2db.impl;
 
 
+import lombok.Data;
 import org.springcat.logcdr.file2db.core.File2DbWorker;
 import org.springcat.logcdr.file2db.core.LocalFile2DbWorker;
 import java.util.List;
 
 
-public class DemoFile2DbWorker extends LocalFile2DbWorker<DemoEntity> {
+public class DemoFile2DbWorker extends LocalFile2DbWorker<DemoFile2DbWorker.DemoEntity> {
+
+    @Data
+    class DemoEntity {
+        /**
+         * 书单ID
+         */
+        private String sheetId;
+        /**
+         * 身份ID
+         */
+        private String msisdn;
+        /**
+         * 图书ID
+         */
+        private String bookId;
+        /**
+         * 操作类型
+         */
+        private String optType;
+        /**
+         * 操作时间
+         */
+        private String optTime;
+    }
 
 
     public DemoFile2DbWorker(String localFilePath) {
--- src/main/java/org/springcat/logcdr/file2db/impl/Test.java
+++ src/main/java/org/springcat/logcdr/file2db/impl/Test.java
@@ -7,7 +7,7 @@ public class Test {
 
     public static void main(String[] args) {
 
-        LocalFile2DbWorker<DemoEntity> ossFile2DbConf = new DemoFile2DbWorker("/Users/springcat/Downloads/ireadsnsplt00480819X20220510091359.txt");
+        LocalFile2DbWorker<DemoFile2DbWorker.DemoEntity> ossFile2DbConf = new DemoFile2DbWorker("/Users/springcat/Downloads/ireadsnsplt00480819X20220510091359.txt");
 
         File2DbFactory.start(ossFile2DbConf);
         System.out.println();
